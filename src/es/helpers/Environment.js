4da2de04b6754c0928efcafdd93b6c2afab58a17
Alexander Artemenko <svetlyak.40wt@gmail.com>
2023-10-07 16:27:05 +0300

    Base URL was fixed to use api on the same server.

--- src/es/helpers/Environment.js
+++ src/es/helpers/Environment.js
@@ -11,7 +11,9 @@
 class EnvironmentClass {
   constructor () {
     // https://github.com/Weedshaker/event-driven-web-components-realworld-example-app/blob/master/FRONTEND_INSTRUCTIONS.md#using-the-hosted-api
-    this._fetchBaseUrl = 'https://conduit.productionready.io/api/'
+    // Original URL      
+    // this._fetchBaseUrl = 'https://conduit.productionready.io/api/'
+    this._fetchBaseUrl = '/realworld/api/'
 
     /**
      * it seems as the conduit example always limits by 10 articles per page
